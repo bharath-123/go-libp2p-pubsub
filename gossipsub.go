3dcd2e511483026c5054a53c7ef26caee6566ca6
Bharath Vedartham <vedabharath12345@gmail.com>
2025-09-29 13:44:44 +0530

    add metrics w.r.t rpc splitting and dropping

--- gossipsub.go
+++ gossipsub.go
@@ -1536,6 +1536,7 @@ func (gs *GossipSubRouter) sendRPC(p peer.ID, out *RPC, urgent bool, msgIDs []st
 	}
 
 	// Potentially split the RPC into multiple RPCs that are below the max message size
+	hasRpcBeenSplit := false
 	for rpc := range out.split(gs.p.maxMessageSize) {
 		if rpc.Size() > gs.p.maxMessageSize {
 			// This should only happen if a single message/control is above the maxMessageSize.
@@ -1543,12 +1544,18 @@ func (gs *GossipSubRouter) sendRPC(p peer.ID, out *RPC, urgent bool, msgIDs []st
 			continue
 		}
 		gs.doSendRPC(&rpc, p, q, urgent, msgIDs)
+		hasRpcBeenSplit = true
+	}
+
+	if hasRpcBeenSplit {
+		gs.p.metrics.rpcSplitCount.Add(context.Background(), 1)
 	}
 }
 
 func (gs *GossipSubRouter) doDropRPC(rpc *RPC, p peer.ID, reason string) {
 	gs.logger.Debug("dropping message to peer", "peer", p, "reason", reason)
 	gs.tracer.DropRPC(rpc, p)
+	gs.p.metrics.rpcsDropped.Add(context.Background(), 1)
 	// push control messages that need to be retried
 	ctl := rpc.GetControl()
 	if ctl != nil {
--- metrics.go
+++ metrics.go
@@ -78,6 +78,9 @@ type metrics struct {
 	outGoingPriorityRpcQueueSize metric.Int64Histogram
 	// the size of the normal outgoing rpc queue
 	outGoingNormalRpcQueueSize metric.Int64Histogram
+	rpcsDropped                metric.Int64Counter
+	// number of times a rpc had to be split
+	rpcSplitCount              metric.Int64Counter
 
 	duplicateMessages        metric.Int64Counter
 	rejectedMessages         metric.Int64Counter
@@ -138,6 +141,20 @@ func InitMetrics(ps *PubSub) error {
 		metricPrefix+"outgoing_normal_rpc_queue_size",
 		metric.WithDescription("The size of the normal outgoing rpc queue"),
 		metric.WithExplicitBucketBoundaries(1, 5, 10, 20, 30, 50, 70, 100, 500, 1000),
+	); err != nil {
+		return err
+	}
+
+	if ps.metrics.rpcSplitCount, err = meter.Int64Counter(
+		metricPrefix+"rpc_split_count",
+		metric.WithDescription("The number of times a rpc had to be split"),
+	); err != nil {
+		return err
+	}
+
+	if ps.metrics.rpcsDropped, err = meter.Int64Counter(
+		metricPrefix+"rpcs_dropped",
+		metric.WithDescription("The number of times a rpc had to be dropped"),
 	); err != nil {
 		return err
 	}
